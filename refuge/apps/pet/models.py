1f9ea4fc38c804d852289710a6fe6f261bec91e8
Daniel Santos <dfsantosbu@unal.edu.co>
2017-12-24 13:51:18 -0500

    Create first model PET.

--- refuge/apps/pet/models.py
+++ refuge/apps/pet/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Pet(models.Model):
+    name = models.CharField(max_length=30)
+    genre = models.CharField(max_length=10)
+    age = models.IntegerField()
+    rescue_date = models.DateField()
+    
