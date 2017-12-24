2c1d203824dd80ee1235bbbaa10848c8f20ffca5
Daniel Santos <dfsantosbu@unal.edu.co>
2017-12-24 18:29:55 -0500

    ADd apps' urls to the main urls.

--- refuge/apps/pet/urls.py
+++ refuge/apps/pet/urls.py
@@ -3,5 +3,4 @@ from django.urls import path
 from apps.pet.views import index
 urlpatterns = [
     path('', index),
-    path('index/', index),
 ]
--- refuge/refuge/urls.py
+++ refuge/refuge/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.pet.urls')),
+    path('pet/', include('apps.pet.urls')),
+    path('adopt/', include('apps.adoption.urls')),
 ]
