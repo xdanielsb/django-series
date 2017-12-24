3eb6d77baa9425e0376e74da8d3bfc4cf5229fe2
Daniel Santos <dfsantosbu@unal.edu.co>
2017-12-24 15:49:22 -0500

    Add relations between entities and create model Vaccine.

--- refuge/apps/pet/models.py
+++ refuge/apps/pet/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from apps.adoption.models import Person
+
+class Vaccine(models.Model):
+    name = models.CharField(max_length=10)
 
 class Pet(models.Model):
     name = models.CharField(max_length=30)
     genre = models.CharField(max_length=10)
     age = models.IntegerField()
     rescue_date = models.DateField()
-    
+    owner = models.ForeignKey(Person, null = True, blank = True, on_delete=models.CASCADE)
+    vaccines = models.ManyToManyField(Vaccine)
